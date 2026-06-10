1d9cea6b15b842fe652b553467755a45d28d1ba0
satoha-claydesign <claydesign.soil@gmail.com>
2026-06-10 23:47:24 +0900

    初回コミット：FastAPIチュートリアル環境構築

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from typing import Optional
+
+app = FastAPI()
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
